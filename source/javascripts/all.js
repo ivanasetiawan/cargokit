f04246a8938960ae719a34d1d70b37070eb0a70e
Ivana Setiawan <ivana@funda.nl>
2016-05-07 22:29:46 +0200

    pre set up

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+$(document).ready(function () {
+	var isCustomSelect = '[data-custom-select]';
+    $(isCustomSelect).change(function () {
+        var str = "";
+        str = $(this).find(":selected").text();
+        $(this).next(".out").text(str);
+    }).trigger('change');
+})
