8d178cb63d91ba39a942f51e1fada5766de07972
Ivana Setiawan <ivana@funda.nl>
2016-05-16 12:01:57 +0200

    Clean up form - starting tracking with documentations.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,11 +4,21 @@ $(document).ready(function () {
 	// Focus on parent div
 	if ($('[data-get-focus]').length) {
 		$('[data-get-focus]').focus(
-		    function(){
-		        $(this).parents('[data-get-focus-parent]').addClass('focusing');
-		    }).blur(
-		    function(){
-		        $(this).parents('[data-get-focus-parent]').removeClass('focusing');
-		    });
-	}
-})
+			function(){
+				$(this).parents('[data-get-focus-parent]').addClass('focusing');
+			}).blur(
+			function(){
+				$(this).parents('[data-get-focus-parent]').removeClass('focusing');
+			});
+	};
+
+	// Custom radio input
+	$('[data-custom-radio]').on('change', function() {
+		$('[data-custom-radio]').closest('[data-custom-radio-parent]').removeClass('active');
+
+		if ($(this).is(':checked')) {
+			$(this).closest('[data-custom-radio-parent').addClass('active');
+		}
+
+	});
+});
